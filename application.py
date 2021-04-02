36de0c5b58c215fd0f20093848998901144f2bd5
snaybanban <36524932+snaybanban@users.noreply.github.com>
2021-04-02 16:48:47 -0600

    completado

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import os, requests
+import os, requests, json, datetime
 from helpers import login_required
 
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -24,7 +24,6 @@ Session(app)
 
 # Set up database
 engine = create_engine("postgres://ajigwrxxtsfjgh:eef2bb35cd85a37e7d10b5699794a552f7dd4736560b44c88126e9f1228f8f51@ec2-54-145-102-149.compute-1.amazonaws.com:5432/d8tvv4bh4q66pr")
-#engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))  
 
 @app.route("/")
@@ -37,38 +36,31 @@ def index():
 def login():
     """Log user in"""
 
-    # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             flash("Usuario vacio")
             return render_template("login.html")
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             flash("Contraseña vacia")
             return render_template("login.html")    
         
 
-        # Remember which user has logged in
         rows=db.execute("SELECT * FROM users WHERE username=:username", {"username":request.form.get("username")})
         rows=rows.fetchone()
         print(rows)
-        ####check contraseña
+        #check contraseña
         if rows == None or not check_password_hash(rows[2], request.form.get("password")):
             print('error: contraseña incorrecta o usuario no registrado')
             return render_template("error.html")
        
-        # Redirect user to home page
         print(rows[1])
         session["user_id"] = rows[0]
         return render_template("index.html")
 
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
@@ -77,10 +69,8 @@ def login():
 def logout():
     """Log user out"""
 
-    # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
     return redirect(url_for("index"))
 
 
@@ -89,35 +79,56 @@ def logout():
 def register():
     """Register user"""
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             flash("username vacio")
             return render_template("register.html")
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             flash("password vacio")
             return render_template("register.html")
 
-        # Ensure password and confirmation match
         elif not request.form.get("password") == request.form.get("confirmation"):
             flash("No coincide")
             return render_template("register.html")
 
-        # hash the password and insert a new user in the database
-        paswordhash = generate_password_hash(request.form.get("confirmation"))
-        rows = db.execute("INSERT INTO users (username, hashs) VALUES(:username, :paswordhash)",{"username":request.form.get("username"), "paswordhash":paswordhash})
+        username = request.form.get("username")
+        paswordhash = generate_password_hash(request.form.get("password"))
+        rows = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+        print(rows)
+        if rows:
+            flash("El usuario ya existe")
+            return render_template("register.html")
+        else:
+            row2 = db.execute("INSERT INTO users (username, hashs) VALUES(:username, :password)", {"username": username, "password": paswordhash})
+            db.commit()
+            flash("Usuario registrado")
+            return render_template("login.html")
+    return render_template("register.html")
 
+@app.route("/cambio", methods=["POST","GET"])
+@login_required
+def cambio():
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return render_template("cambio.html")
+        elif not request.form.get("password"):
+            return render_template("cambio.html")
+        elif not request.form.get("confirmation"):
+            return render_template("cambio.html")
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return render_template("cambio.html")
+
+           
+        query = db.execute("update users SET username = :username, hashs = :hashs WHERE id_user = :user_id",{"username":request.form.get("username") , "hashs":generate_password_hash(request.form.get("password")) , "user_id":session["user_id"]})
         db.commit()
-
-        return render_template("login.html")
+        session.clear()
+        flash("La Contraseña se a cambiado")
         
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("register.html")
+        return render_template("login.html")
+                    
+    return render_template("cambio.html")
 
 @app.route("/busqueda", methods=["GET", "POST"])
 @login_required
@@ -141,19 +152,20 @@ def busqueda():
 @login_required
 def book(isbn):
     if request.method == "POST":
-        usuario = session["id"]
-
+        usuario = session["user_id"]
         rating = int(request.form.get("rating"))
-        comment = ( request.form.get("reviews"))
-        
-        row = db.execute("SELECT * FROM reviews WHERE id_user = :user_id AND isbn = :isbn",{"user_id":session["id"], "isbn":isbn})
+        comment = request.form.get("comment")
+        print("ESTE ES EL COMENTARIO", comment)
+        row = db.execute("SELECT * FROM reviews WHERE id_user = :user_id AND isbn = :isbn",{"user_id":session["user_id"], "isbn":isbn})
 
         print(row)
 
-        if rows.rowcount == 1:
+        if row.rowcount == 1:
             flash("usted ya realizo un comentario a este libro")
             return redirect("/book/"+isbn)
-        query = db.execute("INSERT INTO reviews (isbn,id_user,rating,comment,time) VALUES (:isbn,:user_id,:rating,:comment,now())",{"isbn":isbn, "user_id":usuario, "rating":rating, "comment":comment})
+        fecha = datetime.datetime.now()
+        print("Esta es la fecha de hoy", fecha)
+        query = db.execute("INSERT INTO reviews (isbn,id_user,rating,comment,time) VALUES (:isbn,:user_id,:rating,:comment,:date)", {"isbn":isbn, "user_id":usuario, "rating":rating, "comment":comment, "date": datetime.datetime.now()})
         db.commit()
         
         flash("Su comentario se publico correctamente")
@@ -167,15 +179,38 @@ def book(isbn):
         contenido = (contenido['items'][0])
         contenido = contenido['volumeInfo']
 
-        print(contenido)
-        print("")
-        print(contenido['ratingsCount'])
+        descripcion = contenido['description']
+        imagen = contenido['imageLinks']
+    return render_template("book.html", query=query, review=review, imagen=imagen, contenido=contenido, descripcion=descripcion)
 
+@app.route("/api/<isbn>", methods=["GET"])
+@login_required
+def api(isbn):
 
-        
-        print("")
-        print(contenido['averageRating'])
+    if 'user_id' in session:
 
-        imagen= contenido['imageLinks']
-        print(imagen['thumbnail'])
-    return render_template("book.html", query = query, review = review,imagen = imagen, contenido = contenido)
+        query1 = db.execute(
+            "SELECT * FROM books where isbn = :isbn", {"isbn": isbn}).fetchall()
+        print("Este es el query 1", query1)
+        print("Este es el isbn 1", query1[0]['isbn'])
+        if query1 == None:
+            return ("No se encuentra")
+
+        query2 = db.execute("SELECT COUNT(reviews) as cantidad_reviews,  AVG(reviews.rating) as promedio_puntiacion FROM reviews where isbn = :isbn", {"isbn": isbn}).fetchall()
+
+        isbn = query1[0]['isbn'],
+        title = query1[0]['title'],
+        author = query1[0]['author'],
+        year = query1[0]['year'],
+        cantidad_reviews = query2[0]['cantidad_reviews'],
+        promedio_puntiacion = [float('%.2f' % (query2[0]['promedio_puntiacion']))]
+
+        requests = {
+            "isbn": isbn,
+            "title": title,
+            "author": author,
+            "year": year,
+            "cantidad_reviews": cantidad_reviews,
+            "promedio_puntiacion": promedio_puntiacion
+        }
+        return jsonify(requests)
